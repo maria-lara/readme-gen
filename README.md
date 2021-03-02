a56a78b50294e6b6a3e97ace43fc9673b2f0237d
Maria Lara <74436232+maria-lara@users.noreply.github.com>
2021-03-01 18:23:48 -0800

    pushing js code and package

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# readme-gen
+# test
+    ![License](https://img.shields.io/badge/License-Apache-2.0-blue)
+
+    ## Description
+    tst
+
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Contributing](#contribution)
+    * [Testing](#testing)
+    * [License](#license)
+    * [Questions](#questions)
+
+    ## Installation Instructions
+    tst
+
+    ## How-To
+    ste
+
+    ## Contributors
+    s
+
+    ## Testing 
+    s
+    
+    ## License
+    License: Apache-2.0.
+
+    ## Questions
+    * For questions, feedback, or comments, please contact index@gmail.com.
+
+    * Feel free to check out more of my work at: https://github/com//.
---
+++ index.js
@@ -0,0 +1,79 @@
+// Inquirer for input //
+const inquirer = require('inquirer');
+
+// Generate Markdown file //
+const createMarkdown = require('./utils/createMarkdown');
+// util for promises //
+const util = require('util');
+
+// fs to create file //
+const fs = require('fs');
+
+
+// Variables for prompting questions and inputs using inquirer prompt //
+const inputPrompts = ()=> {
+    return inquirer
+    .prompt([
+        // Add questions to create readme //
+        {
+            type: 'input',
+            message: 'What is the title of this project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'What is the description of this project?',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What are the installation instructions for this project?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'How does one use this projec?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'What are your contribution guidelines for this project?',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'Please outline the testing instructions.',
+            name: 'testing',
+        },
+        {
+            type: "list",
+            message: "What license would you like to add for this project?",
+            name: "license",
+            choices: ["Apache-2.0", "MIT"]
+        },
+        {
+            type: 'input',
+            message: 'Please enter your GitHub username.',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'Please enter your email address.',
+            name: 'email',
+        }
+    ])
+};
+
+// Function to create the README //
+const createFile = util.promisify(fs.writeFile);
+
+// Then init function // 
+const init = ()=> {
+    inputPrompts()
+        .then((data) => createFile('README.md', createMarkdown(data)))
+        .then(() => console.log('Congratulations, you have successfully created your README file!'))
+        .catch((err) => console.log(err));
+}
+
+// Call init function //
+init();
